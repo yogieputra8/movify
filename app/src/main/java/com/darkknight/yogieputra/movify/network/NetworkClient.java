cb4079d566c3ad8e56d74e8a13f8e40293253bc3
yogie.putra8 <yogie.putra8@gmail.com>
2018-11-15 22:00:58 +0700

    fix network client

--- app/src/main/java/com/darkknight/yogieputra/movify/network/NetworkClient.java
+++ app/src/main/java/com/darkknight/yogieputra/movify/network/NetworkClient.java
@@ -1,25 +1,28 @@
 package com.darkknight.yogieputra.movify.network;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NetworkClient {
-    public static Retrofit retrofit;
     private final static String BASE_API_URL = "http://api.themoviedb.org/3/";
+    private static Retrofit retrofit;
+    private static Gson gson = new GsonBuilder().create();
 
-    public void NetworkClient(){
-    }
+    private static HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+    private static OkHttpClient.Builder builder = new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor);
+    private static OkHttpClient okHttpClient = builder.build();
 
     public static Retrofit getRetrofit(){
         if (retrofit == null){
-            OkHttpClient.Builder builder = new OkHttpClient.Builder();
-            OkHttpClient okHttpClient = builder.build();
-
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_API_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(gson))
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .client(okHttpClient)
                     .build();
